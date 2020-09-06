cec92ec72e707dcf2f17a59df573cfc2d6e58702
Asineth <u0@asineth.gq>
2020-09-06 15:29:38 -0400

    1.1.1 Release
    What's new?
    * Now supports odysseyra1n script from Coolstar.
    * Recompresses odysseyra1n assets to save space (-20MB).
    * Fixed some issues that would prevent iproxy from working.
    * Added a shell for debugging.
    * Fancy welcome screen has been added.
    * It boots marginally faster now.
    * Fixed GRUB errors during boot due to missing modules.

--- build.sh
+++ build.sh
@@ -7,37 +7,40 @@ VERSION="1.1.0"
 ROOTFS="http://dl-cdn.alpinelinux.org/alpine/v3.12/releases/x86_64/alpine-minirootfs-3.12.0-x86_64.tar.gz"
 CRBINARY="https://assets.checkra.in/downloads/linux/cli/x86_64/607faa865e90e72834fce04468ae4f5119971b310ecf246128e3126db49e3d4f/checkra1n"
 
-umount rootfs/dev > /dev/null 2>&1
-umount rootfs/sys > /dev/null 2>&1
-umount rootfs/proc > /dev/null 2>&1
+# clean up previous attempts
+umount -v work/rootfs/dev >/dev/null 2>&1
+umount -v work/rootfs/sys >/dev/null 2>&1
+umount -v work/rootfs/proc >/dev/null 2>&1
 rm -rf work
-mkdir -p work/rootfs work/iso/boot/grub
+mkdir -pv work/{rootfs,iso/boot/grub}
 cd work
 
+# fetch rootfs
 curl -sL "$ROOTFS" | tar -xzC rootfs
-
-mount -o bind /dev rootfs/dev
-mount -t sysfs sysfs rootfs/sys
-mount -t proc proc rootfs/proc
+mount -vo bind /dev rootfs/dev
+mount -vt sysfs sysfs rootfs/sys
+mount -vt proc proc rootfs/proc
 cp /etc/resolv.conf rootfs/etc
-
 cat << ! > rootfs/etc/apk/repositories
 http://dl-cdn.alpinelinux.org/alpine/edge/main
 http://dl-cdn.alpinelinux.org/alpine/edge/community
 http://dl-cdn.alpinelinux.org/alpine/edge/testing
 !
 
+# rootfs packages & services
 cat << ! | chroot rootfs /usr/bin/env PATH=/usr/bin:/bin:/usr/sbin:/sbin /bin/sh
 apk upgrade
-apk add alpine-base ncurses-terminfo-base udev usbmuxd
+apk add alpine-base ncurses-terminfo-base udev usbmuxd openssh-client sshpass usbutils
 apk add --no-scripts linux-lts linux-firmware-none
 rc-update add bootmisc
 rc-update add hwdrivers
+rc-update add networking
 rc-update add udev
 rc-update add udev-trigger
 rc-update add udev-settle
 !
 
+# kernel modules
 cat << ! > rootfs/etc/mkinitfs/features.d/checkn1x.modules
 kernel/drivers/usb/host
 kernel/drivers/hid/usbhid
@@ -47,45 +50,73 @@ kernel/drivers/hid/hid-apple.ko
 !
 chroot rootfs /usr/bin/env PATH=/usr/bin:/bin:/usr/sbin:/sbin \
 	/sbin/mkinitfs -F "checkn1x" -k -t /tmp -q $(ls rootfs/lib/modules)
-
-umount rootfs/dev
-umount rootfs/sys
-umount rootfs/proc
-rm -f rootfs/etc/resolv.conf
-
-rm -rf rootfs/lib/modules
-mv rootfs/tmp/lib/modules rootfs/lib
-find rootfs/lib/modules/* -type f -name "*.ko" -exec strip --strip-unneeded {} \;
-find rootfs/lib/modules/* -type f -name "*.ko" -exec xz -v -T 0 -9 {} \;
+rm -rfv rootfs/lib/modules
+mv -v rootfs/tmp/lib/modules rootfs/lib
+find rootfs/lib/modules/* -type f -name "*.ko" | xargs -n1 -P`nproc` -- strip -v --strip-unneeded
+find rootfs/lib/modules/* -type f -name "*.ko" | xargs -n1 -P`nproc` -- xz --x86 -v9eT0
 depmod -b rootfs $(ls rootfs/lib/modules)
 
-ln -s ../../etc/terminfo rootfs/usr/share/terminfo
-curl -sLo rootfs/usr/bin/checkra1n "$CRBINARY"
-chmod 755 rootfs/usr/bin/checkra1n
+# unmount fs
+umount -v rootfs/dev
+umount -v rootfs/sys
+umount -v rootfs/proc
 
-cp ../inittab rootfs/etc
-ln -s sbin/init rootfs/init
+# fetch resources
+curl -Lo rootfs/usr/local/bin/checkra1n "$CRBINARY"
+mkdir -pv rootfs/opt/odysseyra1n && pushd $_
+curl -LO 'https://github.com/coolstar/odyssey-bootstrap/raw/master/bootstrap_1500-ssh.tar.gz' \
+	-O 'https://github.com/coolstar/odyssey-bootstrap/raw/master/bootstrap_1600-ssh.tar.gz' \
+	-O 'https://github.com/coolstar/odyssey-bootstrap/raw/master/org.coolstar.sileo_1.8.1_iphoneos-arm.deb' \
+	-O 'https://github.com/coolstar/odyssey-bootstrap/raw/master/migration'
+find . -type f -name '*.gz' | xargs -n1 -P`nproc` -- gzip -vd
+find . -type f -name '*.tar' | xargs -n1 -P`nproc` -- xz -v --arm -9 -e -T0
+popd
 
-cp rootfs/boot/vmlinuz-lts iso/boot/vmlinuz
+# copy files
+cp -av ../inittab rootfs/etc
+cp -av ../scripts/* rootfs/usr/local/bin
+chmod -v 755 rootfs/usr/local/bin/*
+ln -sv sbin/init rootfs/init
+ln -sv ../../etc/terminfo rootfs/usr/share/terminfo # fix ncurses
+echo 'auto lo' > rootfs/etc/network/interfaces # fix 127.0.0.1
+
+# boot config
+cp -av rootfs/boot/vmlinuz-lts iso/boot/vmlinuz
 cat << ! > iso/boot/grub/grub.cfg
 insmod all_video
+echo 'checkn1x $VERSION : https://asineth.gq'
 linux /boot/vmlinuz quiet loglevel=3
 initrd /boot/initramfs.xz
 boot
 !
 
-cd rootfs
+# initramfs
+pushd rootfs
 rm -rfv tmp/*
-rm -rfv var/cache/*
 rm -rfv boot/*
-find . | cpio -oH newc | xz -z -C crc32 --x86 -9 -e -T 0 > ../iso/boot/initramfs.xz
-cd ..
+rm -rfv var/cache/*
+rm -fv etc/resolv.conf
+find . | cpio -oH newc | xz -C crc32 --x86 -vz9eT0 > ../iso/boot/initramfs.xz
+popd
 
+# iso creation
+part_acorn.mod
+part_amiga.mod
+part_apple.mod
+part_bsd.mod
+part_dfly.mod
+part_dvh.mod
+part_gpt.mod
+part_msdos.mod
+part_plan.mod
+part_sun.mod
+part_sunpc.mod
+GRUB_MODS="linux all_video configfile echo part_acorn part_amiga part_apple part_bsd part_dfly part_dvh part_gpt part_msdos part_plan part_sun part_sunpc"
 grub-mkrescue -o "checkn1x-$VERSION.iso" iso \
 	--compress=xz \
 	--fonts= \
-	--install-modules="linux all_video configfile" \
-	--modules="linux all_video configfile" \
+	--install-modules="$GRUB_MODS" \
+	--modules="$GRUB_MODS" \
 	--locales= \
 	--themes= \
 	--verbose
